4b4052d2c7400b38043676ebcf2172f87198d623
Abdullah Raseheed <abdullah.rasheed452@gmail.com>
2025-09-14 03:33:59 +0500

    updated FourteenthActivity.kt

--- app/src/main/java/com/example/assignment1/FourteenthActivity.kt
+++ app/src/main/java/com/example/assignment1/FourteenthActivity.kt
@@ -1,12 +1,21 @@
 package com.example.assignment1
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FourteenthActivity : AppCompatActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val goToFifteenth = Runnable {
+        startActivity(Intent(this, FifteenthActivity::class.java))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,20 @@ class FourteenthActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(goToFifteenth, 3000L)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(goToFifteenth)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(goToFifteenth)
     }
 }
