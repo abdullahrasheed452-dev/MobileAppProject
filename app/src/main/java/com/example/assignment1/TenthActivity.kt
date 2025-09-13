90951dd137843035175071972b540545b42ea092
Abdullah Raseheed <abdullah.rasheed452@gmail.com>
2025-09-13 14:31:37 +0500

    Updated Tenthpage

--- app/src/main/java/com/example/assignment1/TenthActivity.kt
+++ app/src/main/java/com/example/assignment1/TenthActivity.kt
@@ -1,6 +1,8 @@
 package com.example.assignment1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class TenthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tenthpage)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnEndCall = findViewById<ImageButton>(R.id.btnEndCall)
+
+        btnEndCall.setOnClickListener {
+            val intent = Intent(this, EleventhActivity::class.java)
+            startActivity(intent)
         }
     }
 }
