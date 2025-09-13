53ff88eba04eeae0a396fae0eeb5fcdd14c1e3cf
Abdullah Raseheed <abdullah.rasheed452@gmail.com>
2025-09-13 12:20:52 +0500

    Added splash screen layout and logic

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.firstpage)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Delay for 3 seconds, then open SecondActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: finish first page so user can't go back
+        }, 3000) // 3000ms = 3 seconds
+    }
+}
