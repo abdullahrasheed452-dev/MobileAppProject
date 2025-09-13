1e884da89a3a2dd21682a3cc7c89998a1c25f8c3
Abdullah Raseheed <abdullah.rasheed452@gmail.com>
2025-09-13 17:34:25 +0500

    updated EleventhActivity.kt

--- app/src/main/java/com/example/assignment1/EleventhActivity.kt
+++ app/src/main/java/com/example/assignment1/EleventhActivity.kt
@@ -1,12 +1,20 @@
 package com.example.assignment1
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class EleventhActivity : AppCompatActivity() {
+    private val handler = Handler(Looper.getMainLooper())
+    private val goToTwelfth = Runnable {
+        startActivity(Intent(this, TwelfthActivity::class.java))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,21 @@ class EleventhActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.removeCallbacks(goToTwelfth)
+        handler.postDelayed(goToTwelfth, 3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(goToTwelfth)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(goToTwelfth)
     }
 }
