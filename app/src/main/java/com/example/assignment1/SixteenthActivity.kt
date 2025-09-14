acd9a336f2b75827a56c6e56e49c28bd09a5d8fb
Abdullah Raseheed <abdullah.rasheed452@gmail.com>
2025-09-14 17:34:58 +0500

    updated SixteenthActivity.kt

---
+++ app/src/main/java/com/example/assignment1/SixteenthActivity.kt
@@ -0,0 +1,26 @@
+package com.example.assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SixteenthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.sixteenthpage)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<TextView>(R.id.btnNext).setOnClickListener {
+            startActivity(Intent(this, SeventeenthActivity::class.java))
+        }
+    }
+
+}
